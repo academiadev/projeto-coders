80ae26268c3c941c67c654e667c4f4cbb7d4f293
Fuerback <felipefuerback@hotmail.com>
2018-05-02 17:26:05 -0300

    Ajustes
    Corrigindo erro no console para carregar imagem;
    
    Lendo dados do javascript;

--- front-end/controller/dashboard.js
+++ front-end/controller/dashboard.js
@@ -2,16 +2,23 @@ var dashboardModulo = angular.module('dashboardModulo', []);
 
 dashboardModulo.controller('dashboardController', function ($scope){
   $scope.gastosUsuario = [
-    {descricao:'Visita', status:'waiting', valor:'312.00', categoria:'Outros', usuario:'Felipe'},
-    {descricao:'Almoço', status:'approved', valor:'215.00', categoria:'Alimentação', usuario:'Willian'},
-    {descricao:'Hotel', status:'canceled', valor:'312.00', categoria:'Hospedagem', usuario:'Kauan'},
-    {descricao:'Uber', status:'canceled', valor:'40.00', categoria:'Transporte', usuario:'Bruno'}
+    {descricao:'Visita', status:'waiting', valor:'312,00', categoria:'Outros', usuario:'Felipe', data:''},
+    {descricao:'Almoço', status:'approved', valor:'215,00', categoria:'Alimentação', usuario:'Willian', data:''},
+    {descricao:'Hotel', status:'canceled', valor:'312,00', categoria:'Hospedagem', usuario:'Kauan', data:''},
+    {descricao:'Uber', status:'canceled', valor:'40,00', categoria:'Transporte', usuario:'Bruno', data:''}
   ];
   $scope.gastosTotalUsuario = [
-    {valor:'312.00', email:'fuerback@gmail.com', usuario:'Felipe'},
-    {valor:'215.00', email:'willian@gmail.com', usuario:'Willian'},
-    {valor:'315.00', email:'kauan@gmail.com', usuario:'Kauan'},
-    {valor:'415.00', email:'bruno@gmail.com', usuario:'Bruno'},
+    {valor:'312,00', email:'fuerback@gmail.com', usuario:'Felipe'},
+    {valor:'215,00', email:'willian@gmail.com', usuario:'Willian'},
+    {valor:'315,00', email:'kauan@gmail.com', usuario:'Kauan'},
+    {valor:'415,00', email:'bruno@gmail.com', usuario:'Bruno'}
+  ];
+    
+  $scope.gastosCategoria = [
+    {nome:'Hospedagem', codigo: 1},
+    {nome:'Alimentação', codigo: 2}, 
+    {nome:'Transporte', codigo: 3},
+    {nome:'Outros', codigo: 4}
   ];
 
   $scope.showModal = function() {
@@ -22,8 +29,8 @@ dashboardModulo.controller('dashboardController', function ($scope){
     $('.sidenav').sidenav();
   }
 
-  $scope.fechaModal = function() {
-    $('.modal').modal().close();
+  $scope.salvaReembolso = function(reembolso) {
+    $scope.gastosUsuario.push({descricao: reembolso.nome, status: 'waiting', valor: input.val().replace('R$', ''), categoria: reembolso.categoria.nome, usuario: 'Felipe', data: reembolso.data});
   }
 
   $scope.copiarCodigoEmpresa = function() {
@@ -31,6 +38,12 @@ dashboardModulo.controller('dashboardController', function ($scope){
     //pegar texto do botao e copiar pro clipboard
     M.toast({html: 'Código da empresa copiado!', classes: 'rounded'});
   }
+
+  var input = $("#valor").maskMoney({
+    prefix: "R$ ",
+    decimal: ",",
+    thousands: "."
+	});
 })
 
 $(document).ready(function(){
@@ -38,11 +51,5 @@ $(document).ready(function(){
   $('.datepicker').datepicker({
     dateFormat: 'dd/mm/yy'
   });
-
-  $("#valor").maskMoney({
-    prefix: "R$ ",
-    decimal: ",",
-    thousands: "."
-	});
 });
 
