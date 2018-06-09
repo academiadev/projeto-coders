2b095b9dd1eb04d8eff45247aa502ed95d41a2fe
nobreconfrade <willpereirabr@gmail.com>
2018-06-09 16:26:34 -0300

    encoder da senha

---
+++ src/main/java/br/com/academiadev/projetocoders/reembolsocoders/config/jwt/ApiPasswordEncoder.java
@@ -0,0 +1,36 @@
+package br.com.academiadev.projetocoders.reembolsocoders.config.jwt;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiPasswordEncoder implements PasswordEncoder {
+
+    private PasswordEncoder passwordEncoder;
+
+    @Value("${jwt.pass_secret}")
+    private String PASS_SECRET;
+
+    public ApiPasswordEncoder() {
+        super();
+    }
+
+    public ApiPasswordEncoder(BCryptPasswordEncoder passwordEncoder) {
+        super();
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return passwordEncoder.encode(PASS_SECRET + rawPassword);
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(PASS_SECRET + rawPassword, encodedPassword);
+    }
+
+}
+
