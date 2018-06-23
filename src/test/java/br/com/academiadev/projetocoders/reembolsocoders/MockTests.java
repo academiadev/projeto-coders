3778fdae5152d08f97406a9990f9bae51f8640a6
nobreconfrade <willpereirabr@gmail.com>
2018-06-23 17:18:35 -0300

    testes de mockup

--- src/test/java/br/com/academiadev/projetocoders/reembolsocoders/MockTests.java
+++ src/test/java/br/com/academiadev/projetocoders/reembolsocoders/MockTests.java
@@ -4,9 +4,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.charset.Charset;
+import java.time.LocalDate;
 
+import br.com.academiadev.projetocoders.reembolsocoders.dto.ReembolsoDTO;
+import br.com.academiadev.projetocoders.reembolsocoders.dto.UsuarioDTO;
+import br.com.academiadev.projetocoders.reembolsocoders.exception.ReembolsoNaoAguardandoException;
+import br.com.academiadev.projetocoders.reembolsocoders.exception.ReembolsoNaoEncontradoException;
+import br.com.academiadev.projetocoders.reembolsocoders.model.Categoria;
 import br.com.academiadev.projetocoders.reembolsocoders.model.Reembolso;
+import br.com.academiadev.projetocoders.reembolsocoders.model.StatusReembolso;
+import br.com.academiadev.projetocoders.reembolsocoders.model.Usuario;
 import br.com.academiadev.projetocoders.reembolsocoders.repository.ReembolsoRepository;
 import br.com.academiadev.projetocoders.reembolsocoders.service.ReembolsoService;
 import org.junit.Test;
@@ -25,22 +34,17 @@ import static org.mockito.BDDMockito.given;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import br.com.academiadev.bluerefund.dto.CadastroAdminDTO;
-import br.com.academiadev.bluerefund.service.AdminService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ClientsTests {
-
-	@Autowired
-	private MockMvc mockMvc;
-
-	@MockBean
-	private ReembolsoService reembolsoService;
+public class MockTests {
 
 	@MockBean
 	private ReembolsoRepository reembolsoRepository;
+
+	@Autowired
+	private ReembolsoService reembolsoService;
 
 	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
@@ -51,11 +55,45 @@ public class ClientsTests {
 	}
 
 	@Test
-	public void testBuscarProdutoPorIdOK() throws Exception {
-		given(reembolsoRepository.findOne());
+	public void alterandoStatusReembolso() throws ReembolsoNaoEncontradoException, ReembolsoNaoAguardandoException {
+		Reembolso reembolso = testeReembolso();
+		given(reembolsoRepository.findOne(-1l)).willReturn(reembolso);
 
-
+		reembolsoService.AlterarStatus(-1l,"RECUSADO");
 	}
 
+	@Test
+	public void alterandoStatusReembolsoRecusado() throws ReembolsoNaoEncontradoException, ReembolsoNaoAguardandoException {
+		Reembolso reembolso = testeReembolso();
+		reembolso.setStatus(StatusReembolso.RECUSADO);
+		given(reembolsoRepository.findOne(-1l)).willReturn(reembolso);
+
+		reembolsoService.AlterarStatus(-1l,"RECUSADO");
+	}
+
+	@Test
+	public void alterandoStatusReembolsoNaoEncontrado() throws ReembolsoNaoEncontradoException, ReembolsoNaoAguardandoException {
+		Reembolso reembolso = testeReembolso();
+		reembolso.setStatus(StatusReembolso.RECUSADO);
+		given(reembolsoRepository.findOne(-1l)).willReturn(reembolso);
+
+		reembolsoService.AlterarStatus(-2l,"RECUSADO");
+	}
+
+
+	public Reembolso testeReembolso(){
+		Reembolso reembolso = new Reembolso();
+		reembolso.setValor(new BigDecimal(369));
+		reembolso.setArquivoPath("PLACEHOLDER PATH");
+		reembolso.setCategoria(Categoria.OUTROS);
+		reembolso.setData(LocalDate.now());
+		reembolso.setDescricao("PLACEHOLDER DESCRICAO");
+		reembolso.setUsuario(new Usuario());
+		reembolso.setDataEnviado(LocalDate.now());
+		reembolso.setExcluido(false);
+		reembolso.setStatus(StatusReembolso.AGUARDANDO);
+		reembolso.setId(-1l);
+		return reembolso;
+	}
 
 }
